c18a2d5836e216ea3d47bc99a470f911b4dedcf4
cazyw <kazeisc@gmail.com>
2017-07-30 16:21:32 +1000

    refactor js code to split into short functions

--- js/wikipedia.js
+++ js/wikipedia.js
@@ -1,14 +1,22 @@
-function jsonlength(obj) {
-    return Object.keys(obj).length;
-}
-
-
 function showError(errorMsg) {
     $(".errorBox").css('visibility','visible').html(errorMsg);
 }
 
 function hideError(){
     $(".errorBox").css('visibility','hidden').html("no errors"); 
+}
+
+function buildResultBox(link, title, extract){
+    let html = "";
+    html += "<a href=\"" + link + "\" target=\"_blank\">";
+    html += "<div class='resultsBox panel panel-default'>";
+    html += "<div class='panel-heading'>";
+    html += "<h3 class='panel-title'>" + title + "</h3>"
+    html += "</div>";
+    html += "<div class='panel-body'>" + extract + "</div>";
+    html += "</div>";
+    html += "</a>";
+    return html;
 }
 
 function getWikiPage(term){
@@ -32,15 +40,7 @@ function getWikiPage(term){
                 var extract = result[key]["extract"];
                 var page = result[key]["pageid"];
                 var link = "https://en.wikipedia.org/?curid=" + page;
-                console.log(index, ": ", title, " - ", extract);
-                html += "<a href=\"" + link + "\" target=\"_blank\">";
-                html += "<div class='resultsBox panel panel-default'>";
-                html += "<div class='panel-heading'>";
-                html += "<h3 class='panel-title'>" + title + "</h3>"
-                html += "</div>";
-                html += "<div class='panel-body'>" + extract + "</div>";
-                html += "</div>";
-                html += "</a>";
+                html += buildResultBox(link, title, extract);                
             });
             $('.resultList').animate({'margin-top': '0px', 'opacity': 1}, 'slow');        
             $(".resultList").html(html);
@@ -52,15 +52,19 @@ function getWikiPage(term){
     });
 }
 
-function validateInput(value){
-    return (!(value.length === 0));
+function toggleReadOnly(setting) {
+    $("input").attr("readonly", setting);
+    $("#submit").attr("disabled", setting);
 }
 
 function clearPage(){
-    $("input").attr("readonly", false);
-    $("#submit").attr("disabled", false);
+    toggleReadOnly(false);
     $("#search-input").val("");
-    $(".errorBox").css('visibility','hidden');
+    hideError();
+}
+
+function invalidInput(term){
+    return !(/[^ ]+/.test(term)) || term.length === 0 || term === "" || term === undefined || term === "undefined"|| term === " ";
 }
 
 $(document).ready(function(){
@@ -86,32 +90,30 @@ $(document).ready(function(){
         });
 
         var term = $("#search-form :input[name='search-input']")[0]["value"];
-        if (!(/[A-z0-9]+/.test(term)) || term.length === 0 || term === "" || term === undefined || term === "undefined"|| term === " ") {
-            $(".errorBox").css('visibility','visible');
-            $(".errorBox").html("Did you enter anything?");
+        if (invalidInput(term)) {
+            showError("Did you enter anything?");
         } else {
             window.scrollTo(0, 0);
-            $("input").attr("readonly", true);
-            $("#submit").attr("disabled", true);
-            $(".errorBox").css('visibility','hidden');
+            toggleReadOnly(true);
+            hideError();
             getWikiPage(term);
         }
         
         $('#search-input').mouseenter(function(){
             if($("input").is('[readonly=readonly]')){
-                $(".errorBox").css('visibility','visible').html("Hit the X to clear the search");    
+                showError("Hit the X to clear the search");    
             }
         
         });
         $('#search-input').mouseleave(function(){
              if($("input").is('[readonly=readonly]')){
-                $(".errorBox").css('visibility','hidden');    
+                hideError();    
             }
         });
          $('#search-input').on("click", function(){
              if($("input").is('[readonly=readonly]')){
                 $("input").blur();
-                $(".errorBox").css('visibility','visible').html("Hit the X to clear the search");   
+                showError("Hit the X to clear the search");   
             }
         });       
 
